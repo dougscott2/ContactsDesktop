394a340142e05c2b9b68306f4e3f04c9f0b0eaeb
DrScott <dougscott2@gmail.com>
2015-10-20 13:12:16 -0400

    assignment completed before optional tasks are implemented

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,44 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable{
+
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+    @FXML
+    ListView listView;
+    @FXML
+    TextField nameField;
+    @FXML
+    TextField phoneField;
+    @FXML
+    TextField emailField;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listView.setItems(contacts);
+    }
+    public void addContact(){
+        if (!nameField.getText().equals("") && !phoneField.getText().equals("") && !emailField.getText().equals("")) {
+            Contact contact = new Contact(nameField.getText(), phoneField.getText(), emailField.getText());
+            contacts.add(contact);
+        }
+           nameField.setText(""); //sets text field to blank after addItem has been clicked
+           phoneField.setText("");
+           emailField.setText("");
+    }
+    public void removeContact(){
+        Contact contact = (Contact) listView.getSelectionModel().getSelectedItem();
+        contacts.remove(contact);
+    }
 }
